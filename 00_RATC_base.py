111b8b87df32c985b88a71a7fadc77bd55b72a21
heada9217 <99290797+heada9217@users.noreply.github.com>
2022-05-26 14:49:33 +1200

    Add files via upload

---
+++ 00_RATC_base.py
@@ -0,0 +1,16 @@
+#import statements
+
+
+
+
+#functions go here
+
+#Checks that user has entered yes / no to a question 
+
+#Main Routine goes here
+
+    #Ask user for instructions
+
+    #Ask user whether they want to use Radians or Degrees
+
+    #Ask user for wanted 
