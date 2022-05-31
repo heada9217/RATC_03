5df5c79350ae979028d1158529a9b0912c37d2bc
heada9217 <99290797+heada9217@users.noreply.github.com>
2022-06-01 09:59:27 +1200

    Add files via upload

--- 00_RATC_base.py
+++ 00_RATC_base.py
@@ -13,4 +13,8 @@
 
     #Ask user whether they want to use Radians or Degrees
 
-    #Ask user for wanted 
+    #Ask user whether they have 1 or 2 sides
+
+    #Ask which side user wants (AC,AB,BC)
+
+    #If user only has 1 side, ask user for given angle 
